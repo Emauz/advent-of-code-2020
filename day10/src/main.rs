4a9553cd69a075d13ff6a51e4b8c78a416761f4c
Eric Moss <eric@ericmoss.com>
2020-12-10 22:07:57 -0500

    Completed day 10 part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -5,6 +5,7 @@
 use std::env;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
+use std::cmp::min;
 
 fn main() {
     // initialize file IO
@@ -14,6 +15,8 @@ fn main() {
     let reader = BufReader::new(file);
 
     let mut series: Vec<usize> = Vec::new();
+    // add starting outlet to seies (0)
+    series.push(0);
     // parse input file into series data
     for line in reader.lines().map(|l| l.unwrap()){
         let number = line.parse().expect("Unable to parse number");
@@ -24,7 +27,7 @@ fn main() {
     series.push(series.last().unwrap() + 3);
 
     println!("{}", part1(&series));
-    //println!("{}", part2(&series).expect("Unable to get result for part 2"));
+    println!("{}", part2(&series));
 }
 
 fn part1(series: &Vec<usize>) -> usize {
@@ -40,4 +43,28 @@ fn part1(series: &Vec<usize>) -> usize {
         prev_jolts = *adapter;
     }
     num_1s * num_3s
+}
+
+fn part2(series: &Vec<usize>) -> usize {
+    // create vector to keep track of each adapter's valid configs so far
+    let mut configs: Vec<usize> = vec![0; series.len()];
+    configs[0] = 1; // one valid way to get to starting socket
+    for (idx, current_jolts) in series.iter().enumerate(){
+        let end_idx = min(idx + 4, series.len()); // watching out for array bounds
+        //print!("{}|", current_jolts);
+        // for all adapters in the 3 following our current one
+        for next_idx in idx+1..end_idx {
+            // determine if we can reach adapter from our current one
+            let next_jolts = series[next_idx];
+            if next_jolts > current_jolts + 3 {
+                break;
+            }
+            // subsequent adapter can be reached by all valid subconfigs of current adapter
+            configs[next_idx] += configs[idx];
+            //print!("({}, {})", next_jolts, configs[next_idx]);
+        }
+        //println!();
+    }
+    let total_configs = configs.pop().unwrap();
+    total_configs
 }
