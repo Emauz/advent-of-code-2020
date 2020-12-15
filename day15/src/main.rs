8e997210ec245095cf2e0d8437b2be8e2647bd31
Eric Moss <eric@ericmoss.com>
2020-12-15 14:03:41 -0500

    Completed day 15 part 1

---
+++ day15/src/main.rs
@@ -0,0 +1,50 @@
+// Advent of code 2020
+// day 15
+// Eric Moss
+
+use std::env;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std::collections::HashMap;
+
+fn main() {
+    // initialize file IO
+    let filename = env::args().nth(1)
+                              .expect("No filename given");
+    let file = File::open(filename).expect("Unable to open file");
+    let reader = BufReader::new(file);
+
+    let line = reader.lines()
+                     .next()
+                     .unwrap()
+                     .unwrap();
+    let starting_numbers: Vec<usize> = line.split(',')
+                                           .map(|x| x.parse().unwrap())
+                                           .collect();
+
+    println!("{}", part1(&starting_numbers));
+    //println!("{}", part2(&instructions));
+}
+
+fn part1(starting_numbers: &Vec<usize>) -> usize {
+    let last_starting_num: usize = starting_numbers[starting_numbers.len() - 1];
+    let starting_numbers = &starting_numbers[0 .. starting_numbers.len() - 1];
+    let mut spoken_record: HashMap<usize, usize> = HashMap::new();
+    for (i, num) in starting_numbers.iter().enumerate() {
+        //println!("{}: {}", i, num);
+        spoken_record.insert(*num, i);
+    }
+    let mut next_number: usize = starting_numbers.len() - spoken_record.get(&last_starting_num)
+                                                                       .unwrap_or(&starting_numbers.len());
+    spoken_record.insert(last_starting_num, starting_numbers.len());
+    //println!("{}: {}", starting_numbers.len(), last_starting_num);
+    for i in (starting_numbers.len() + 1) .. 2019 {
+        let current_number = next_number;
+        //println!("{}: {}", i, next_number);
+        next_number = i - spoken_record.get(&next_number)
+                                       .unwrap_or(&i);
+        spoken_record.insert(current_number, i);
+    }
+    next_number
+}
+
