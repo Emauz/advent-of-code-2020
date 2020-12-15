e3b737a5203070ccaab42999d9f1fc7779b13c42
Eric Moss <eric@ericmoss.com>
2020-12-15 14:39:21 -0500

    Optimized hashmap lookups

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -31,19 +31,17 @@ fn part1(starting_numbers: &Vec<usize>, target_num: usize) -> usize {
     let starting_numbers = &starting_numbers[0 .. starting_numbers.len() - 1];
     let mut spoken_record: HashMap<usize, usize> = HashMap::new();
     for (i, num) in starting_numbers.iter().enumerate() {
-        //println!("{}: {}", i, num);
         spoken_record.insert(*num, i);
     }
     let mut next_number: usize = starting_numbers.len() - spoken_record.get(&last_starting_num)
                                                                        .unwrap_or(&starting_numbers.len());
     spoken_record.insert(last_starting_num, starting_numbers.len());
-    //println!("{}: {}", starting_numbers.len(), last_starting_num);
     for i in (starting_numbers.len() + 1) .. target_num - 1 {
         let current_number = next_number;
-        //println!("{}: {}", i, next_number);
-        next_number = i - spoken_record.get(&next_number)
-                                       .unwrap_or(&i);
-        spoken_record.insert(current_number, i);
+        next_number = match spoken_record.insert(current_number, i) {
+            Some(n) => {i - n},
+            None => 0,
+        }
     }
     next_number
 }
