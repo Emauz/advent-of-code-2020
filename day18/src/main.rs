efe233d847c750865fac20e7de145f9542b747f1
Eric Moss <eric@ericmoss.com>
2020-12-19 16:28:25 -0500

    Completed day 18 part 2

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -22,16 +22,15 @@ fn main() {
     let file = File::open(filename).expect("Unable to open file");
     let reader = BufReader::new(file);
 
-    let mut homework: VecDeque<Expression> = VecDeque::new();
+    let mut input: Vec<String> = Vec::new();
     // parse all lines into operation trees
     for line in reader.lines().map(|l| l.unwrap()) {
-        let mut tokens: VecDeque<char> = line.chars().filter(|c| !c.is_whitespace()).collect();
-        let parsed_expression = parse_tokens(&mut tokens);
-        homework.push_back(*parsed_expression);
+        let line: String = line.chars().filter(|c| !c.is_whitespace()).collect();
+        input.push(line);
     }
 
-    println!("{}", part1(&homework));
-    //println!("{}", part2(&space_4d));
+    println!("{}", part1(&input));
+    println!("{}", part2(&input));
 }
 
 fn evaluate_expression(expr: &Expression) -> u64 {
@@ -71,23 +70,82 @@ fn parse_tokens(tokens: &mut VecDeque<char>) -> Box<Expression> {
         return right_expr;
     }
 
-    let left_expr: Box<Expression> = parse_tokens(tokens);
-
     // construct aritmhetic expression and return
     match arithmetic_operation {
         '+' => {
+            let left_expr: Box<Expression> = parse_tokens(tokens);
             Box::new(Expression::Add(left_expr, right_expr))
         },
         '*' => {
+            let left_expr: Box<Expression> = parse_tokens(tokens);
             Box::new(Expression::Mul(left_expr, right_expr))
         },
-        _ => {panic!("Parsed unexpected arithmetic operation");},
+        _ => {
+            panic!("Parsed unexpected arithmetic operation");
+        },
     }
+
+
 }
 
+fn parenthesize_multiplication(tokens: &mut VecDeque<char>) -> VecDeque<char> {
+    loop {
+        // continue parenthesizing multiplication until none are left
+        match tokens.iter().position(|c| *c == '+') {
+            Some(idx) => {
+                let mut l_idx = idx - 1;
+                let mut r_idx = idx + 1;
+                tokens[idx] = 't';
+                // insert right parenthesis
+                let mut right_depth = 0;
+                loop {
+                    if tokens[r_idx] == '(' {
+                        right_depth += 1;
+                    }
+                    if tokens[r_idx] == ')' {
+                        right_depth -= 1;
+                    }
+                    if right_depth <= 0 {
+                        // insert parenthesis here!
+                        tokens.insert(r_idx+1, ')');
+                        break;
+                    }
+                    r_idx += 1;
+                }
+                // insert left parenthesis
+                let mut left_depth = 0;
+                loop {
+                    if tokens[l_idx] == ')' {
+                        left_depth += 1;
+                    }
+                    if tokens[l_idx] == '(' {
+                        left_depth -= 1;
+                    }
+                    if left_depth <= 0 {
+                        // insert parenthesis here!
+                        tokens.insert(l_idx, '(');
+                        break;
+                    }
+                    l_idx -= 1;
+                }
+            }
+            None => {
+                break;
+            }
+        }
+    }
+    // return all addition signs to a + (they've been replaced with 't')
+    let tokens: VecDeque<char> = tokens.iter().map(|c| if *c == 't' {'+'} else {*c}).collect();
+    tokens
+}
 
-
-fn part1(homework: &VecDeque<Expression>) -> u64 {
+fn part1(input: &Vec<String>) -> u64 {
+    let mut homework: Vec<Expression> = Vec::new();
+    for equation in input {
+        let tokens: VecDeque<char> = equation.chars().collect();
+        let parsed_expression = parse_tokens(&mut tokens.clone());
+        homework.push(*parsed_expression);
+    }
     let mut homework_sum = 0;
     for problem in homework.iter() {
         homework_sum += evaluate_expression(problem);
@@ -95,3 +153,18 @@ fn part1(homework: &VecDeque<Expression>) -> u64 {
     homework_sum
 }
 
+fn part2(input: &Vec<String>) -> u64 {
+    let mut homework: Vec<Expression> = Vec::new();
+    for equation in input {
+        let mut tokens: VecDeque<char> = equation.chars().collect();
+        // parenthesize multiplication first!
+        tokens = parenthesize_multiplication(&mut tokens);
+        let parsed_expression = parse_tokens(&mut tokens.clone());
+        homework.push(*parsed_expression);
+    }
+    let mut homework_sum = 0;
+    for problem in homework.iter() {
+        homework_sum += evaluate_expression(problem);
+    }
+    homework_sum
+}
